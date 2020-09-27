9b30f534531618facbc51c6483d2c6c73b4690d6
parthk3004 <pkorawala@gmail.com>
2020-09-28 03:57:14 +0530

    java programming in depth knowledge

---
+++ src/firstno.java
@@ -0,0 +1,12 @@
+public class firstno {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        int myfirstno = 5;
+        int myfirst = 5+10;
+        int myfirstno1 = (5+10) +(2*10);
+        System.out.println(myfirstno);
+        System.out.println(myfirst);
+        System.out.println(myfirstno1);
+
+    }
+}
